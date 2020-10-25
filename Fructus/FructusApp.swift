1f88070e1877ea6b549789dbc822693270146314
Shimamura Satoshi <shimamura.satoshi.jri@outlook.jp>
2020-10-25 13:37:53 +0900

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 推進 dx on 2020/10/25.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
